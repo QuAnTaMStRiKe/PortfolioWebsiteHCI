c4297d43772f421e52f0a4f00d2f70ae1c3750b2
QuAnTaMStRiKe <quantamstrike@gmail.com>
2023-02-12 19:33:12 -0500

    please stop this

--- portfolioWebsite/settings.py
+++ portfolioWebsite/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['https://dry-anchorage-46694.herokuapp.com/']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
